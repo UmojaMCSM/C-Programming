0fae4d2b1cb8df38332771d7ce97977f934f141c
UmojaMCSM <kwoolard3@schools.nyc.gov>
2025-12-12 09:37:01 -0500

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,10 @@
+//Find the output of the following program
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("\n Enter two four digit numbers: ");
+    scanf("%2d %4d, &a, &b);
+    printf("\n The two numbers are: %d adn %d", a, b);
+    return 0;
+}
